fe84e4c995b9ddabe8a28f62b4d111aaefd3c7f3
Er4nces <159928013+Juanchoscor@users.noreply.github.com>
2024-02-20 13:10:13 -0500

    Clase 20/02
    Cancha y Puntos

--- Assets/Scripts/Ball Controller.cs
+++ Assets/Scripts/Ball Controller.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     public bool onGround;
+    public float fuerza_y;
     // Start is called before the first frame update
     void Start()
     {
         onGround = false;
+        fuerza_y=0;
     }
 
     // Update is called once per frame
@@ -25,9 +27,17 @@ public class BallController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             if (onGround){
-                Vector3 fuerza = new Vector3(0,500,0);
+                Vector3 fuerza = new Vector3(0,500,fuerza_y);
                 GetComponent<Rigidbody>().AddForce(fuerza);
             }
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            fuerza_y = fuerza_y -10;
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+          fuerza_y = fuerza_y +10;
         }
 
     }
